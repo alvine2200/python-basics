d987880c848b51f5b1942b647a0b5b591894e69b
alvine2200 <alvinellavu@gmail.com>
2022-10-13 09:14:12 +0300

    still on list reverse

---
+++ maxminnum.py
@@ -0,0 +1,24 @@
+def maximum(first, second):
+    big = max(first, second)
+    return big
+
+
+first = eval(input('Enter first number:'))
+second = eval(input('Enter another number:'))
+print(maximum(first, second))
+
+def minimum(first,second):
+    small=min(first,second)
+    return small
+
+
+first = eval(input('Enter first number:'))
+second = eval(input('Enter another number:'))
+print(minimum(first,second))
+
+list=[8,9,4,5,2,0]
+print(max(list))
+
+
+
+
